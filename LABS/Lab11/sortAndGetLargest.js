3e8d8f85f21003b4a5a56f3c53b6cc9acfcafbb2
Joshua Liang <joshcliang@yahoo.com>
2018-10-17 13:44:31 -0700

    Complete Part 2

--- LABS/Lab11/sortAndGetLargest.js
+++ LABS/Lab11/sortAndGetLargest.js
@@ -1,15 +1,20 @@
 function swap(arr,i,j) {
-  tmp = arr[i]; arr[i] = arr[j]; arr[j] = tmp;
+"use strict";
+  var tmp = arr[i]; arr[i] = arr[j]; arr[j] = tmp;
 }
 function sortAndGetLargest (arr) {
-  tmp = arr[0]; // largest elem
-  for (i=0; i<arr.length; i++) {
-    if (arr[i] > tmp) tmp = arr[i];
-    for (j=i+1; j<arr.length; j++)
-      if (arr[i] < arr[j]) swap(arr,i,j);
-  }
+"use strict";
+  var tmp = arr[0]; // largest elem
+  var i;
+  var j;
+  for (i=0; i<arr.length; i+=1) {
+    if (arr[i] > tmp){
+ tmp = arr[i];}
+    for (j=i+1; j<arr.length; j+=1){
+      if (arr[i] < arr[j]){ 
+swap(arr,i,j);}
+  }}
   return tmp;
 }
 var largest = sortAndGetLargest([99,2,43,8,0,21,12]);
 console.log(largest); // should be 99, but prints 0
-

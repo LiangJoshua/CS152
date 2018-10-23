021d21e8f1a0de5478eda7a73a7b60bc710761d3
Joshua Liang <joshcliang@yahoo.com>
2018-10-23 16:19:12 -0700

    Update LABS/Lab12/trace.js

--- LABS/Lab12/trace.js
+++ LABS/Lab12/trace.js
@@ -1,63 +1,95 @@
-// Modify the forwarding API to log all actions taken on an object.
-
-var print = console.log
-
-
 function handlerMaker(obj) {
-	return {
-		delete: function(name) {
-			print(" > delete() " + name)
-			return delete obj[name]
-		},
-		has: function(name) {
-			print(" > has() " + name)
-			return name in obj
-		},
-		hasOwn: function(name) {
-			print(" > hasOwn() " + name)
-			return Object.prototype.hasOwnProperty.call(obj, name)
-		},
-		get: function(receiver, name) {
-			val = obj[name]
-			print(" > get() " + name + " -> " + val)
-			return val
-		},
-		set: function(receiver, name, val) {
-			print(" > set() " + name + " <- " + val)
-			obj[name] = val
-			return true
-		},
-		enumerate: function() {
-			var result = []
-			for (name in obj)
-				result.push(name)
-			print(" > enumerate() [" + result + "]")
-			return result
-		},
-		keys: function() {
-			print(" > keys() [" + Object.keys(obj) + "]")
-			return Object.keys(obj)
-		}
-	}
+    return {
+        // Fundamental traps
+        deleteProperty: function (target, name) {
+            if (name in target) {
+                delete target[name];
+                console.log('property removed: ' + name);
+            }
+
+        },
+        getOwnPropertyDescriptor: function (name) {
+            var desc = Object.getOwnPropertyDescriptor(obj, name);
+
+            if (desc !== undefined) {
+                desc.configurable = true;
+            }
+            return desc;
+
+        },
+        getPropertyDescriptor: function (name) {
+            var desc = Object.getPropertyDescriptor(obj, name);
+            desc.configurable = true;
+            if (desc !== undefined) {
+                desc.configurable = true;
+            }
+            return desc;
+        },
+
+        getOwnPropertyNames: function () {
+
+            return Object.getOwnPropertyNames(obj);
+        },
+        getPropertyNames: function () {
+            return Object.getPropertyNames(obj);
+        },
+        defineProperty: function (name, desc) {
+            Object.defineProperty(obj, name, desc);
+        },
+
+
+        fix: function () {
+
+            if (Object.isFrozen(obj)) {
+                return Object.getOwnPropertyNames(obj).map(function (name) {
+                    return Object.getOwnPropertyDescriptor(obj, name);
+                });
+            }
+
+            return undefined;
+        },
+
+        // derived traps
+        has: function (name) {
+            console.log('testing if ' + name + ' is in ' + obj);
+            return name in obj;
+        },
+        hasOwn: function (name) {
+            console.log('testing if ' + obj + ' has ' + name + ' as a property.');
+            return Object.prototype.hasOwnProperty.call(obj, name);
+        },
+        get: function (receiver, name) {
+            console.log('retrieving ' + obj[name] + ' from ' + obj + '[' + name + ']');
+            return obj[name];
+        },
+        set: function (receiver, name, val) {
+            console.log('setting ' + '[' + name + '] to ' + val);
+            obj[name] = val;
+            return true;
+        }, // bad behavior when set fails in non-strict mode
+
+        keys: function () {
+            console.log('fetching keys from ' + obj);
+            return Object.keys(obj)
+        }
+    };
 }
 
-// ...
 
-var o = Object.create({});
-var proxy = new Proxy({},(handlerMaker(o)));
+var constantVals = {
+    pi: 3.14,
+    e: 2.718,
+    goldenRatio: 1.30357
+};
 
-proxy.blabla = 12; // Thanks to the forwarding, o now has a 'blabla' property with a value of 12
-o.blabla++; // just incrementing o.blabla
+var proxy = new Proxy(constantVals, handlerMaker(constantVals));
 
-print(proxy.blabla); // prints 13: the getting operation is forwarded to o which returns 13.
-
-proxy.a = 3
-proxy.b = 2
-proxy.c = 1
-
-
-
-delete(proxy.c)
-
+proxy.a = 1;
+proxy.b = 2;
+proxy.e;
+proxy.c = 3;
+delete proxy.c;
+proxy.c;
+'pi' in proxy;
 
 

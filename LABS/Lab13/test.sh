0f997cf7e6ee8133c1235b9f6c8b5b8f3ff9d9b9
Joshua Liang <joshcliang@yahoo.com>
2018-10-29 14:27:15 -0700

    Update LABS/Lab13/test.sh

--- LABS/Lab13/test.sh
+++ LABS/Lab13/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Update jar file to match your system
-ANTLR_LIB="/usr/local/lib/antlr-4.7.1-complete.jar"
+ANTLR_LIB="/Users/macadmin/Desktop/GitHub/CS152/LABS/Lab13/antlr-4.7.1-complete.jar"
 touch out
 rm -r out
 mkdir out
@@ -8,5 +8,3 @@ java -jar ${ANTLR_LIB} -no-listener -visitor Expr.g4 -o out/
 cp *.java out/
 javac -cp .:out:${ANTLR_LIB} out/*.java
 java -classpath .:out/:${ANTLR_LIB} Calc $1
-
-
